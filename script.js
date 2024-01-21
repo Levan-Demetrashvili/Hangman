ac68d7a92f32ff5615cbfe3f6b6cb0dc469ca706
tankistttt <levandemetrashvili10@gmail.com>
2024-01-21 19:53:51 +0400

    feat: canvas hangman

--- script.js
+++ script.js
@@ -7,8 +7,46 @@ const guesses = document.getElementById('word');
 const btnHint = document.getElementById('hint');
 const btnReset = document.getElementById('reset');
 
-// starting condition
+//* Canvas
+/** @type {HTMLCanvasElement} */
 
+const canvas = document.getElementById('stickman');
+const ctx = canvas.getContext('2d');
+const width = canvas.width;
+const height = canvas.height;
+ctx.fillStyle = 'white';
+ctx.strokeStyle = 'white';
+ctx.lineWidth = 2;
+
+function drawVector(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.stroke();
+}
+const act1 = () => ctx.fillRect(0, height, width / 2, -1);
+const act2 = () => ctx.fillRect(10, 0, 2, height);
+const act3 = () => ctx.fillRect(0, 0, width / 4, 2);
+const act4 = () => ctx.fillRect(width / 4 - 10, 2, 2, 10);
+
+const act5 = () => {
+  ctx.beginPath();
+  ctx.lineWidth = 3;
+  ctx.arc(width / 4 - 10, 22, 10, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.closePath();
+};
+ctx.lineWidth = 2;
+
+const act6 = () => ctx.fillRect(width / 4 - 10, 32, 2, 40);
+const act7 = () => drawVector(65, 46, 22, 54);
+const act8 = () => drawVector(65, 46, 104, 54);
+const act9 = () => drawVector(66, 71, 22, 100);
+const act10 = () => drawVector(66, 71, 108, 100);
+
+const reservedActions = [act1, act2, act3, act4, act5, act6, act7, act8, act9, act10];
+
+// starting condition
 const categoriesArr = ['football Premier league Teams', 'Films', 'Cities'];
 const chosenCategoryArr = [
   ['everton', 'liverpool', 'swansea', 'chelsea', 'hull', 'manchester-city', 'newcastle-united'],
@@ -28,7 +66,7 @@ const hintsArr = [
   ['Science-Fiction horror film', '1971 American action film', 'Historical drama', 'Anamated Fish', 'Giant great white shark'],
   ['Northern city in the UK', 'Home of AC and Inter', 'Spanish capital', 'Netherlands capital', 'Czech Republic capital'],
 ];
-let randomCategoryNum, category, randomWordNum, word, counterLives, lives, guessesArr;
+let randomCategoryNum, category, randomWordNum, word, counterLives, lives, guessesArr, actions;
 
 function removeClasses() {
   for (let letter of lettersbtns) {
@@ -53,6 +91,8 @@ function addUnderScores() {
 }
 
 function init() {
+  ctx.clearRect(0, 0, width, height);
+  actions = [...reservedActions];
   counterLives = 10;
   livesText.innerHTML = '';
   livesText.outerHTML = `<p class="lives-text">You Have <span id="lives"> 10 </span> lives</p>`;
@@ -85,8 +125,9 @@ for (let letter of lettersbtns) {
       }
       // letter wrong
       else {
+        // action[0]() -> actions.shift()();
+        actions.shift()();
         counterLives--;
-        console.log(counterLives);
         lives.textContent = counterLives > 0 ? counterLives : (livesText.textContent = 'Game Over');
       }
     }
@@ -96,5 +137,4 @@ btnHint.addEventListener('click', function () {
   const hint = hintsArr[randomCategoryNum][randomWordNum];
   clue.textContent = hint;
 });
-
 btnReset.addEventListener('click', init);
